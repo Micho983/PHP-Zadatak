eefd2b55f9fac14d0d49662deb6d20e39e9ef794
krunoslav.lojen@gmail.com <krunoslav.lojen@gmail.com>
2024-02-28 22:09:16 +0100

    zavrsno

---
+++ obrazac.php
@@ -0,0 +1,30 @@
+<body>   
+    <table border="1">
+        <form action="">
+            <tr>
+                <td>IME:</td>
+                <td><input type="text" name="IME">
+                </td>
+            </tr>
+            <tr>
+                <td>PREZIME:</td>
+                <td><input type="text" name="PREZIME">
+                </td>
+            </tr>
+            <tr>
+                <td>EMAIL:</td>
+                <td><input type="email" name="EMAIL" id="">
+                </td>
+            </tr>   
+            <tr>
+                <td>LOZINKA</td>
+                <td><input type="password" name="LOZINKA" id=""></td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <button>  Pošalji  !</button>
+                </td>
+            </tr>
+        </form>
+</table>
+</body>
